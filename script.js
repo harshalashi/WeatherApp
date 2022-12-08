cdfe7346c8b54d8b0e670fc0c1b0c53ec084e8de
Harsh <imharshalashi333@gmail.com>
2022-12-08 23:10:01 +0530

    Project Complete

--- script.js
+++ script.js
@@ -1,15 +1,86 @@
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '273c475a67msh47f27fbcc4ef092p1d3427jsn9659a56131d5',
-    'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com',
-  },
-};
+const wrapper = document.querySelector('.wrapper'),
+  inputPart = wrapper.querySelector('.input-part'),
+  infoTxt = wrapper.querySelector('.info-txt'),
+  inputField = wrapper.querySelector('input'),
+  locationBtn = inputPart.querySelector('button'),
+  wIcon = wrapper.querySelector('.weather-part img'),
+  arrowBack = wrapper.querySelector('header i');
 
-fetch(
-  'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Badlapur',
-  options
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+let api;
+
+inputField.addEventListener('keyup', (e) => {
+  if (e.key == 'Enter' && inputField.value != '') {
+    requestApi(inputField.value);
+  }
+});
+
+locationBtn.addEventListener('click', () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  } else {
+    alert('Your Browser do not support Geolocation API');
+  }
+});
+
+function onSuccess(position) {
+  const { latitude, longitude } = position.coords;
+  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=885d71bccad3fb81d06c45288ce7b105`;
+  fetchData();
+}
+function onError(error) {
+  infoTxt.innerText = error.message;
+  infoTxt.classList.add('error');
+}
+
+function requestApi(city) {
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=885d71bccad3fb81d06c45288ce7b105`;
+  fetchData();
+}
+
+function fetchData() {
+  infoTxt.innerText = 'Getting Weather Details...';
+  infoTxt.classList.add('pending');
+  fetch(api)
+    .then((response) => response.json())
+    .then((result) => weatherDetails(result));
+}
+
+function weatherDetails(info) {
+  infoTxt.classList.replace('pending', 'error');
+  if (info.cod == '404') {
+    infoTxt.innerText = `${inputField.value} is not a valid City name`;
+  } else {
+    const city = info.name;
+    const country = info.sys.country;
+    const { description, id } = info.weather[0];
+    const { feels_like, humidity, temp } = info.main;
+
+    if (id == 800) {
+      wIcon.src = './weather-app-icons/Weather Icons/clear.svg';
+    } else if (id >= 200 && id <= 232) {
+      wIcon.src = './weather-app-icons/Weather Icons/storm.svg';
+    } else if (id >= 300 && id <= 321) {
+      wIcon.src = './weather-app-icons/Weather Icons/rain.svg';
+    } else if (id >= 600 && id <= 622) {
+      wIcon.src = './weather-app-icons/Weather Icons/snow.svg';
+    } else if (id >= 701 && id <= 781) {
+      wIcon.src = './weather-app-icons/Weather Icons/haze.svg';
+    } else if (id >= 801 && id <= 804) {
+      wIcon.src = './weather-app-icons/Weather Icons/cloud.svg';
+    }
+
+    wrapper.querySelector('.temp .numb').innerText = Math.floor(temp);
+    wrapper.querySelector('.weather').innerText = description;
+    wrapper.querySelector('.location span').innerText = `${city}, ${country}`;
+    wrapper.querySelector('.temp .numb-2').innerText = Math.floor(feels_like);
+    wrapper.querySelector('.humidity span').innerText = `${humidity}`;
+
+    infoTxt.classList.remove('pending', 'error');
+    wrapper.classList.add('active');
+    console.log(info);
+  }
+}
+
+arrowBack.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+});
